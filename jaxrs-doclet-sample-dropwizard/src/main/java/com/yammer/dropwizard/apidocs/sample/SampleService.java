fffa655f3465334a83d87ab6390a03ad00209449
Ryan Kennedy <rckenned@gmail.com>
2013-04-23 22:49:27 -0700

    Test for the StackOverflow issue reported with @Auth injected objects.
    I don't see any issues building or with the Swagger UI, so I believe an earlier commit to ignore @Auth parameters has fixed this issue.

--- jaxrs-doclet-sample-dropwizard/src/main/java/com/yammer/dropwizard/apidocs/sample/SampleService.java
+++ jaxrs-doclet-sample-dropwizard/src/main/java/com/yammer/dropwizard/apidocs/sample/SampleService.java
@@ -1,9 +1,15 @@
 package com.yammer.dropwizard.apidocs.sample;
 
+import com.google.common.base.Optional;
 import com.yammer.dropwizard.Service;
+import com.yammer.dropwizard.apidocs.sample.resources.AuthResource;
 import com.yammer.dropwizard.apidocs.sample.resources.GreetingsResource;
 import com.yammer.dropwizard.apidocs.sample.resources.HttpServletRequestResource;
 import com.yammer.dropwizard.assets.AssetsBundle;
+import com.yammer.dropwizard.auth.AuthenticationException;
+import com.yammer.dropwizard.auth.Authenticator;
+import com.yammer.dropwizard.auth.basic.BasicAuthProvider;
+import com.yammer.dropwizard.auth.basic.BasicCredentials;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Configuration;
 import com.yammer.dropwizard.config.Environment;
@@ -20,7 +26,15 @@ public class SampleService extends Service<Configuration> {
 
     @Override
     public void run(Configuration configuration, Environment environment) throws Exception {
+        environment.addProvider(new BasicAuthProvider<>(new Authenticator<BasicCredentials, String>() {
+            @Override
+            public Optional<String> authenticate(BasicCredentials basicCredentials) throws AuthenticationException {
+                return Optional.of("USERNAME");
+            }
+        }, "AuthResource Realm"));
+
         environment.addResource(new GreetingsResource());
         environment.addResource(new HttpServletRequestResource());
+        environment.addResource(new AuthResource());
     }
 }
